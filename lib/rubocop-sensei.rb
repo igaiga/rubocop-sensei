0ba3da49e926c1bb08983b1709ef4b7d314b7fd2
Kuniaki Igarashi <kuniaki.igarashi@garnettech373.com>
2022-12-11 11:03:57 +0900

    init

---
+++ lib/rubocop-sensei.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'rubocop'
+
+require_relative 'rubocop/sensei'
+require_relative 'rubocop/sensei/version'
+require_relative 'rubocop/sensei/inject'
+
+RuboCop::Sensei::Inject.defaults!
+
+require_relative 'rubocop/cop/sensei_cops'
