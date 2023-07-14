adf646ce0fa9d3114dd1093cd1406a6d6311ebb8
Kuniaki Igarashi <kuniaki.igarashi@garnettech373.com>
2023-07-14 10:50:00 +0900

    Write spec for Lecture/ExplainPatternMatchCaseIn

--- spec/rubocop/cop/lecture/explain_pattern_match_case_in_spec.rb
+++ spec/rubocop/cop/lecture/explain_pattern_match_case_in_spec.rb
@@ -1,15 +1,14 @@
 # frozen_string_literal: true
 
-# RSpec.describe RuboCop::Cop::Lecture::ExplainPatternMatchCaseIn, :config do
-#   let(:config) { RuboCop::Config.new }
+RSpec.describe RuboCop::Cop::Lecture::ExplainPatternMatchCaseIn, :config do
+  let(:config) { RuboCop::Config.new }
 
-#   it "case in" do
-#     expect_offense(<<~RUBY)
-#       case [1,2]
-#       in [1,x]
-#       end
-#       ^^^^^^^^^^ case in[...]
-#     RUBY
-#   end
-#   # error: unexpected token kIN になる
-# end
+  it "case in" do
+    expect_offense(<<~RUBY)
+      case [1,2]
+      ^^^^^^^^^^ Lecture/ExplainPatternMatchCaseIn: case in はパターンマッチ[...]
+      in [1,x]
+      end
+    RUBY
+  end
+end
